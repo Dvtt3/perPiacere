4a79bec39d21e923f46785771925565d0c415fd2
Dvtt3 <matteo.daddato.1nito@gmail.com>
2022-02-21 21:58:42 +0100

    sempre più vicini

--- alpha/esame/php/gOauthConf.php
+++ alpha/esame/php/gOauthConf.php
@@ -1,5 +1,5 @@
 <?php
-//require_once("vendor/autoload.php");
+require_once("vendor/autoload.php");
 
 $clientID = "1056870111114-6326c1iijmjgm1md7jtifg4mtrquunlc.apps.googleusercontent.com";
 $clientSecret = "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";
